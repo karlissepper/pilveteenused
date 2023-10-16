aeb368f7ae7d6d25bfe5a6cc252890753fff2adb
karlissepper <114573096+karlissepper@users.noreply.github.com>
2023-10-16 11:36:20 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,15 @@
-<?php
-$servername = "karlissis22-server.mysql.database.azure.com";
-$username = "efhrzwbnmh";
-$password = "88WQ4MQFL7762RN6$";
-$dbname = "karlissis22";
+<?php include "templates/header.php"; ?>
 
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "karlissis22-server.mysql.database.azure.com", "efhrzwbnmh", "88WQ4MQFL7762RN6$", "karlissis22", 3306, MYSQLI_CLIENT_SSL);
+<div class="center-align">
+	<ul>
+		<div class="left-align">
+			<li><a href="read.php"><strong>View Catalog</strong></a></li>
+			<li><a href="insert.php"><strong>Add a Product</strong></a></li>
+			<li><a href="update.php"><strong>Update a Product</strong></a></li>
+			<li><a href="delete.php"><strong>Remove a Product</strong></a></li>
+		</div>
+	</ul>
+</div>
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
-$sql = "SELECT * FROM your_table";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-$conn->close();
-?>
+<?php include "templates/footer.php"; ?>
